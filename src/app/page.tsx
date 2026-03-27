a3836145fb6b22f117c8b4f8d7e5ca624de8d7b3
PC03 <luciocarvalho10@hotmail.com.br>
2026-03-27 17:03:44 -0400

    feat(layout): add `Header`, `Container`, and `footer` components to `RootLayout`
    refactor(page): remove unused `Container`, `Header`, and `footer` components from `Home` page
    
    chore(metadata): update site metadata with new title and template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
 import {SpinLoader} from "@/components/SpinLoader";
 import {Suspense} from "react";
 import {PostsList} from "@/components/PostList";
-import {Container} from "@/components/Container";
-import {Header} from "@/components/Header";
 import {PostFeatured} from "@/components/PostFeatured";
 
 export default function Home() {
     return (
-        <Container >
-            <Header />
-
-            <PostFeatured />
+        <>
+            <Suspense fallback={<SpinLoader />}>
+                <PostFeatured />
+            </Suspense >
 
             <Suspense fallback={<SpinLoader />}>
                 <PostsList />
             </Suspense >
-
-            <footer >
-                <p className="text-6xl font-bold text-center py-8">FOOTER</p >
-            </footer >
-        </Container >
+        </>
     );
 }
