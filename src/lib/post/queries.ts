8d1e9b76d0c01dde404fe7dc1a6ce1922a2b4796
LPC <luciocarvalho10@hotmail.com.br>
2026-03-29 18:35:00 -0400

    feat(posts): handle not found posts in `findPostBySlugCached`
    - Add `notFound` function from `next/navigation` to return 404 when a post is not found by slug.

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,12 +1,21 @@
 import {postRepository} from "@/repositories/post";
 import {cache} from "react";
+import {notFound} from "next/navigation";
 
 export const findAllPublicPostsCached = cache(
     async () => await postRepository.findAllPublic(),
 )
 
 export const findPostBySlugCached = cache(
-    async (slug: string) => await postRepository.findBySlug(slug)
+    async (slug: string) => {
+        const post = await postRepository.findBySlug(slug).catch(
+            () => undefined
+        )
+
+        if (!post) notFound();
+
+        return post;
+    }
 )
 
 export const findPostByIdCached = cache(
