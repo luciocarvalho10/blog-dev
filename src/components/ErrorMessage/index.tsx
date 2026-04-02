f7dd17f61221a46a437da693b91fba84d8d75b5e
LPC <luciocarvalho10@hotmail.com.br>
2026-04-02 05:46:55 -0400

    feat(app): create reusable `ErrorMessage` component and refactor `NotFound`
    - Add `ErrorMessage` component for consistent display of error messages.
    - Refactor `NotFound` page to use `ErrorMessage` with props for cleaner and more modular code.

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,23 @@
+type ErrorMessageProps = {
+    pageTitle: string;
+    contentTitle: string;
+    content: string;
+}
+
+export default function ErrorMessage({ pageTitle, contentTitle, content }: ErrorMessageProps) {
+    return (
+        <>
+            <title>{pageTitle}</title>
+            <div className={' min-h-[320] bg-slate-900 text-slate-100 ' +
+                'mb-16 p-8 ' +
+                'rounded-xl flex items-center ' +
+                'justify-center text-center '
+            } >
+                <div >
+                    <h1 className={'text-7xl/tight mb-4 font-extrabold'} >{contentTitle}</h1 >
+                    <div >{content}</div >
+                </div >
+            </div >
+        </>
+    )
+}
